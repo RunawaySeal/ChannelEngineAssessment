8a21d9817c07ddbd51e4f0fd5976572e4c59a64f
NotYourFriendSorry <andreas42c@gmail.com>
2025-08-10 09:53:43 +0200

    Add product service with method to get top products from orders.

---
+++ ChannelEngineAssessment.Domain/ApplicationServices/Products/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using ChannelEngineAssessment.Domain.Models.Orders;
+using ChannelEngineAssessment.Domain.Models.Products;
+using ChannelEngineAssessment.Domain.Repositories.Orders;
+using Serilog;
+using System.Net;
+
+namespace ChannelEngineAssessment.Domain.ApplicationServices.Products
+{
+  public class ProductService(ILogger logger, IOrderRepo orderRepo) : BaseService(logger)
+  {
+    private readonly IOrderRepo _orderRepo = orderRepo;
+
+    public async Task<IEnumerable<Product>?> GetTopProductsAsync(OrderFilters? filters = null, int count = 5)
+    {
+      try
+      {
+        var orders = await _orderRepo.GetOrdersAsync(filters);
+        var products = orders?.Content.SelectMany(o => o.Lines)
+                                      .GroupBy(ol => ol.MerchantProductNo)
+                                      .Select(ol => new Product()
+                                      {
+                                        TotalQuantitySold = ol.Sum(item => item.Quantity),
+                                        MerchantProductNo = ol.Key,
+                                        Name = ol.First().Description
+                                      })
+                                      .OrderBy(p => p.TotalQuantitySold)
+                                      .Take(count);
+
+        return products;
+      }
+      catch (Exception ex)
+      {
+        SetDomainError(HttpStatusCode.NotFound, ex.Message);
+        return default;
+      }
+    }
+  }
+}
---
+++ ChannelEngineAssessment.Domain/Models/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿using ChannelEngineAssessment.Domain.Models.Orders;
+
+namespace ChannelEngineAssessment.Domain.Models.Products
+{
+  public class Product
+  {
+    public string MerchantProductNo { get; set; }
+    public string Name { get; set; }
+    public int TotalQuantitySold { get; set; }
+  }
+}
