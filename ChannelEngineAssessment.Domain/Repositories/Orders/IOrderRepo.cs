0a99c21b79b12863333a01d7c851e9bcdd24e35b
NotYourFriendSorry <andreas42c@gmail.com>
2025-08-10 09:48:39 +0200

    Add orders repo and service with the ability to get all orders with basic filter logic by status.

---
+++ ChannelEngineAssessment.Domain/Repositories/Orders/IOrderRepo.cs
@@ -0,0 +1,10 @@
+﻿using ChannelEngineAssessment.Domain.Models;
+using ChannelEngineAssessment.Domain.Models.Orders;
+
+namespace ChannelEngineAssessment.Domain.Repositories.Orders
+{
+  public interface IOrderRepo
+  {
+    public Task<Response<Order>?> GetOrdersAsync(OrderFilters? filters);
+  }
+}
---
+++ ChannelEngineAssessment.Domain/Repositories/Orders/OrderRepo.cs
@@ -0,0 +1,26 @@
+﻿using ChannelEngineAssessment.Domain.Models;
+using ChannelEngineAssessment.Domain.Models.Orders;
+using Newtonsoft.Json;
+
+namespace ChannelEngineAssessment.Domain.Repositories.Orders
+{
+  public class OrderRepo : BaseRepo, IOrderRepo
+  {
+    public async Task<Response<Order>?> GetOrdersAsync(OrderFilters? filters)
+    {
+      // Build the URL with the filters
+      var orderApiUrl = $"{BaseUrl}/orders";
+      orderApiUrl += $"?apikey={ApiKey}";
+      if(filters is not null)
+      {
+        orderApiUrl += $"&statuses={string.Join(',', filters.Statuses.Select(s => s.ToString()))}";
+      }
+
+      // Get Orders from the API
+      using var httpClient = new HttpClient();
+      var orderJson = await httpClient.GetStringAsync(orderApiUrl);
+
+      return JsonConvert.DeserializeObject<Response<Order>>(orderJson);
+    }
+  }
+}
