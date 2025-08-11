1a27fe46854046ef3e2b34e561ad7b35a0061628
NotYourFriendSorry <andreas42c@gmail.com>
2025-08-11 14:56:11 +0200

    List Top 5 or less products from orders in progress.

--- ChannelEngineAssessment.ConsoleApp/Program.cs
+++ ChannelEngineAssessment.ConsoleApp/Program.cs
@@ -1,16 +1,21 @@
 ﻿using ChannelEngineAssessment.Domain.ApplicationServices.Orders;
+using ChannelEngineAssessment.Domain.ApplicationServices.Products;
 using ChannelEngineAssessment.Domain.Enums;
 using ChannelEngineAssessment.Domain.Models.Orders;
 using ChannelEngineAssessment.Domain.Repositories.Orders;
+using ChannelEngineAssessment.Domain.Repositories.Products;
 
 var apiKey = "541b989ef78ccb1bad630ea5b85c6ebff9ca3322";
 var baseUrl = "https://api-dev.channelengine.net/api/v2";
 
 //Repositories
 var orderRepo = new OrderRepo() { ApiKey = apiKey, BaseUrl = baseUrl };
+var productsRepo = new ProductRepo() { ApiKey = apiKey, BaseUrl = baseUrl };
+
 
 //Services
 var orderService = new OrderService(null, orderRepo);
+var productService = new ProductService(null, orderRepo, productsRepo);
 
 var orderFilters = new OrderFilters
 {
@@ -28,3 +33,15 @@ foreach (var order in orders.Content)
     Console.WriteLine($"-- Product: {line.Description}, Quantity: {line.Quantity}");
   } 
 }
+
+Console.WriteLine($"------------------------------------------------------");
+
+//List Top 5 Products Sold based on In-Progress Orders
+var products = await productService.GetTopProductsAsync(orderFilters, 5);
+Console.WriteLine($"Top Products");
+foreach (var product in products)
+{
+  Console.WriteLine($"-{product.Name}, GTIN: {product.GTIN}, Total Sold: {product.TotalQuantitySold}");
+}
+
+Console.WriteLine($"------------------------------------------------------");
